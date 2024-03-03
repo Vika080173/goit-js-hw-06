1c6b89a20303ffb7f92ac6fde34066a45daae919
Vika080173 <151369286+Vika080173@users.noreply.github.com>
2024-03-03 13:04:28 +0200

    hw_js_06_1

--- js/task-2.js
+++ js/task-2.js
@@ -1,21 +1,24 @@
 class Storage {
   #items = [];
+
   constructor(items) {
     this.#items = items;
   }
+
   getItems() {
     return this.#items;
   }
+
   addItem(newItem) {
     this.#items.push(newItem);
   }
+
   removeItem(itemToRemove) {
-    if (this.#items.includes(itemToRemove) == true) {
-      this.#items.splice(this.#items.includes(itemToRemove), 1);
+    if (this.#items.indexOf(itemToRemove) > -1) {
+      this.#items.splice(this.#items.indexOf(itemToRemove), 1);
     }
   }
 }
-
 const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem('Droid');
